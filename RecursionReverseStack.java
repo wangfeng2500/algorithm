0a0c2181f81784914fd638cabf793255dca62013
gfwangfeng <gfwangfeng@gf.com.cn>
2020-12-22 19:32:54 +0800

    递归反转栈

--- RecursionReverseStack.java
+++ RecursionReverseStack.java
@@ -13,16 +13,27 @@ import java.util.Stack;
    后，从栈顶到栈底为 1、 2、 3、 4、 5，也就是实现栈中元素的逆序，但是只能用递归函数来实
    现， 不能用其他数据结构。
  */
-public class recursionReverseStack
+public class RecursionReverseStack
 {
-//    1 2 3 4 5
-//    5 4 3 2 1
 
     private Stack<Integer> stack = new Stack<>();
 
 
     // 取栈最底部的元素，并删除（递归实现）
-
+    public static int getAndRemoveBottomData(Stack<Integer> stack)
+    {
+        int top = stack.pop();
+        if(!stack.empty())
+        {
+            int bottom = getAndRemoveBottomData(stack);
+            stack.push(top);
+            return bottom;
+        }
+        else
+        {
+            return top;
+        }
+    }
 
 
     // 然后再递归的清空栈，并插入数据
@@ -30,7 +41,7 @@ public class recursionReverseStack
     {
         if (!stack.empty())
         {
-            int value = stack.pop();
+            int value = getAndRemoveBottomData(stack);
             reverse(stack);
             stack.push(value);
         }
