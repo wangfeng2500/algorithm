736ed8030d404111b64112912865a7053b7adb3e
gfwangfeng <gfwangfeng@gf.com.cn>
2021-07-21 09:21:53 +0800

    链表反转题目集合

--- Link/ReverseSingleLink.java
+++ Link/ReverseSingleLink.java
@@ -1,19 +1,7 @@
 package algorithm.Link;
 
 /**
-反转一个单链表。
-
-        示例:
-
-        输入: 1->2->3->4->5->NULL
-        输出: 5->4->3->2->1->NULL
-        进阶:
-        你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
-
-        作者：力扣 (LeetCode)
-        链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnnhm6/
-        来源：力扣（LeetCode）
-        著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+   反转单链表题目集合
  **/
 class ReverseSingleLink {
 
@@ -32,7 +20,10 @@ class ReverseSingleLink {
         }
     }
 
-    // 迭代反转单链表
+    /*
+     *  题目1，单链表反转，easy难度
+     *  解法：迭代反转
+     */
     public static ListNode reverseList(ListNode head) {
         ListNode prev = null;
         ListNode current = head;
@@ -46,29 +37,38 @@ class ReverseSingleLink {
         return prev;
     }
 
-    // 递归的反转链表，其实这个看的不是很懂
-    // 参考：https://labuladong.gitbook.io/algo/mu-lu-ye-1/mu-lu-ye/di-gui-fan-zhuan-lian-biao-de-yi-bu-fen
+    /*
+     * 题目1，单链表反转，easy难度
+     * 解法：递归
+     * 备注：这个看的不是很懂，参考：https://labuladong.gitbook.io/algo/mu-lu-ye-1/mu-lu-ye/di-gui-fan-zhuan-lian-biao-de-yi-bu-fen
+     * FIXME::没想出来
+     */
     public static ListNode reverseLinkRecursive(ListNode head)
     {
-        if(head == null || head.next == null){
+        if(head.next == null){
             return head;
         }
 
         System.out.print("before: ");
         head.print();
+
         // 遍历到链表尾部
         ListNode newHead = reverseLinkRecursive(head.next);
         System.out.print("after: ");
         newHead.print();
 
-        // 反转
+        // 把反转后的尾节点指向当前节点
         head.next.next = head;
         head.next = null;
 
         return newHead;
     }
 
-    // 反转链表的前N个节点
+
+    /*
+     *  题目2，反转链表的前N个节点，easy难度
+     *  解法：迭代反转
+     */
     public static ListNode reverseN(ListNode head, int n){
         if(head == null){
             return null;
@@ -88,10 +88,13 @@ class ReverseSingleLink {
         return prev;
     }
 
-    // 反转链表的第M到第N区间的节点
-    // 注意：m是头节点或者非头节点时，反转后，再次连接时处理方式不一样，所以为了简便期间，设置一个虚拟头节点
-    // 参考：https://leetcode-cn.com/problems/reverse-linked-list-ii/solution/fan-zhuan-lian-biao-ii-by-leetcode-solut-teyq/
-    // FIXME::这个题我没做出来，看的答案，虚拟节点这个想法很好
+    /*
+     *  题目3，反转链表的第M到第N个节点区间，middle难度
+     *  解法：迭代反转
+     *  参考：https://leetcode-cn.com/problems/reverse-linked-list-ii/solution/fan-zhuan-lian-biao-ii-by-leetcode-solut-teyq/
+     *  FIXME:: 这个题我没做出来，看的答案，虚拟节点这个想法很好，因为m是头节点或者非头节点时，反转后再连接的处理方式是不一样的，非常复杂，增加一个虚拟
+                节点，极大的简化了问题
+     */
     public static ListNode reverseMN(ListNode head, int m, int n){
         ListNode dummyNode = new ListNode();
         dummyNode.next = head;
@@ -111,7 +114,7 @@ class ReverseSingleLink {
         // 反转区间节点
         ListNode reverseHeadNode = pre.next;
         ListNode reverseTailNextNode = right.next;
-//        pre.next = null;
+        //        pre.next = null;
         right.next = null;  // 这里一定要切断，不然就把right后面的也一起反转了
         reverseList(reverseHeadNode);
 
@@ -121,8 +124,12 @@ class ReverseSingleLink {
         return dummyNode.next;
     }
 
-    // reverseMN极端情况，需要便利两遍链表，这里改用头插法，在遍历的过程中，直接把节点插入到反转区的头部来
-    // 参考：https://leetcode-cn.com/problems/reverse-linked-list-ii/solution/fan-zhuan-lian-biao-ii-by-leetcode-solut-teyq/
+    /*
+     *  题目3，反转链表的第M到第N个节点区间，middle难度
+     *  解法：另一种迭代解法
+     *  参考：https://leetcode-cn.com/problems/reverse-linked-list-ii/solution/fan-zhuan-lian-biao-ii-by-leetcode-solut-teyq/
+     *  FIXME: reverseMN极端情况，需要遍历两遍链表，这里改用头插法，在遍历的过程中，直接把节点插入到反转区的头部来
+     */
     public static ListNode reserveMN2(ListNode head, int m, int n){
 
         // 设置 dummyNode 是这一类问题的一般做法
@@ -146,6 +153,46 @@ class ReverseSingleLink {
         return dummyNode.next;
     }
 
+    /*
+     * 题目4：k个一组反转链表，比如1->2->3->4->5, k = 2，则为2->1->4->3->5
+     *                    比如1->2->3->4->5, k = 3, 则为3->2->1->4->5，最后剩余的不足K的节点不反转，hard难度的题目
+     * 参考：https://mp.weixin.qq.com/s/A-dQ9spsP_Iu1Y4iCRP9nA
+     * FIXME:: 这个我也没做出来
+     */
+    public static ListNode reverseKGroup(ListNode head, int k){
+        // 使用递归进行，反转一段，然后这一段的尾节点连接到下一段的头节点
+
+        ListNode a = head;
+        ListNode b = head;
+        for(int i = 0; i < k; ++i){
+            if(b == null){
+                return head; // 不足k个的，不需要反转，base case
+            }
+            b = b.next;
+        }
+        ListNode newHead = reverseList(a,b);
+        a.next = reverseKGroup(b,k);
+        return newHead;
+    }
+
+    /** 反转区间 [a, b) 的元素，注意是左闭右开
+     *  注意：这里只管反转这一段
+     */
+    private static ListNode reverseList(ListNode a, ListNode b)
+    {
+        ListNode pre, cur, nxt;
+        pre = null; cur = a;
+        // while 终止的条件改一下就行了
+        while (cur != b) {
+            nxt = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = nxt;
+        }
+        // 返回反转后的头结点
+        return pre;
+    }
+
     public static void main(String[] args) {
         ListNode node1 = new ListNode();
         node1.val = 1;
@@ -167,13 +214,9 @@ class ReverseSingleLink {
         node5.next = node6;
 
         node1.print();
-        ListNode newList = reserveMN2(node1,1,5);
+//        ListNode newList = reverseKGroup(node1,2);
+        ListNode newList = reverseLinkRecursive(node1);
         newList.print();
     }
-
-
-
-
-
 
 }
